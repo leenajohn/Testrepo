78113c5f2ccdf4e83ac0d7e2c577a8a342b3a672
Leena <leena.aju@gmail.com>
2021-05-15 07:25:32 +0000

    method overriding

---
+++ src/oopsConcept/MethodOverrideParent.java
@@ -0,0 +1,8 @@
+package oopsConcept;
+
+public class MethodOverrideParent {
+	public void display1() {
+		System.out.println("Inside Parent");
+	}
+
+}
--- src/oopsConcept/RunnerClass.java
+++ src/oopsConcept/RunnerClass.java
@@ -3,7 +3,7 @@ package oopsConcept;
 public class RunnerClass {
 	public static void main(String args[])
 	{
-     ClassA  Emp1 = new ClassA(100,"Leena", "XYZ");
+  /*   ClassA  Emp1 = new ClassA(100,"Leena", "XYZ");
     // ClassA Emp1= new ClassA();
      ClassB obj1 = new ClassB();
      obj1.Name = "Leena1";
@@ -15,11 +15,49 @@ public class RunnerClass {
      ClassC obj2 = new ClassC();
      obj2.run();
      
-/*	Emp1.Name = "Leena";
+Emp1.Name = "Leena";
 	Emp1.Salary=1000;
-	Emp1.Orgname = "CISCO";*/
+	Emp1.Orgname = "CISCO";
 	
-	Emp1.Display();
-	}
+	Emp1.Display();*/
+		
+		MethodOverloading obj1 = new MethodOverloading();
+		{
+			 obj1.add(1,2);
+			 obj1.add(1.1,1.3);
+			 obj1.mult(1.1,1.2);
+		 	
+		}
+		
+	   MethodOverrideChild obj2= new MethodOverrideChild();
+	   {
+		   obj2.display1();
+		   //obj2.display(a+b);
+	   }
+	
+	   AbstractChild Obj5 = new AbstractChild();
+	   {
+		   Obj5.method2();
+		   Obj5.method1();
+		   
+	   }
+	   InterfaceChild Obj6 = new InterfaceChild();
+	   Obj6.method1();
+	   Obj6.method2();
+	   Obj6.mthod3();
+	   
+	//   EncapsulationExample obj7 =  new EncapsulationExample();
+	   EncapsulationExample obj7 = null;
+	     obj7.setSalary(1000);
+	     
+	     System.out.println("Salary = "+obj7.getSalary());
+	     
 
-}
+		}
+	   
+	   
+	
+	}
+  
+    
+
